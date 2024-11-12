164d1db48ddb6aff8fcc99ce37392f35f3d268f9
tanvir-robin <tanvirrobin18@cse.pstu.ac.bd>
2024-11-13 00:00:40 +0600

    nodemon dev

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-   res.send('Hello World');
+   res.send('Bkash Payment Gateway Request API');
 });
 
 app.post('/initiate', (req, res) => {
